3573c6224addf8448fbe96981d1240700ee4ab90
魏天尧 <weitianyao@bytedance.com>
2020-08-26 14:29:00 +0800

    feat: 优化代码中存在的性能问题

--- index.js
+++ index.js
@@ -7,7 +7,6 @@
 
 'use strict';
 
-var arrDel = require('arr-del');
 
 /*
  * Flatten javascript objects into a single-depth object
@@ -61,26 +60,28 @@ function getCompFn(asc) {
 	return sortFn
 }
 
+function getObjectValue (obj, attr) {
+	if (attr.indexOf('.') === -1) {
+		return obj[attr]
+	} else {
+		return flattenObject(obj)[attr]
+	}
+}
+
+
 // select array whose attribute matches
-function selObjArr(arr, attr, asc) {
+function selObjArr(arr, attr, asc, ignore) {
 	var outArr = [];
-	var arr2 = JSON.parse(JSON.stringify(arr));
 	var sortFn = getCompFn(asc);
 	if(attr === undefined || arr.length === 0){
 		return [];
 	}
-	if(typeof attr !== "string"){
-		throw new TypeError('PARAM MUST BE STRING');
+	if (!ignore) {
+		arr.sort(function(a,b){return sortFn(getObjectValue(a, attr), getObjectValue(b, attr))});
 	}
-	for(var i=0, len = arr2.length; i<len; i++){
-		if(Object.prototype.toString.call(arr2[i]) !== "[object Object]"){
-			throw new TypeError('PARAM MUST BE OBJECT ARRAY');
-		}
-	}
-	arr2.sort(function(a,b){return sortFn(flattenObject(a)[attr], flattenObject(b)[attr])});
-	var optIndex;
-	for(var i=0, len = arr2.length-1; i<len; i++){
-		if(sortFn(flattenObject(arr2[i])[attr], flattenObject(arr2[i+1])[attr])){
+	var optIndex = 0;
+	for(var i=0, len = arr.length-1; i<len; i++){
+		if(sortFn(getObjectValue(arr[i], attr), getObjectValue(arr[i+1], attr))){
 			optIndex = i;
 			break
 		}
@@ -89,7 +90,7 @@ function selObjArr(arr, attr, asc) {
 			break
 		}
 	}
-	outArr = arr2.slice(0, optIndex+1);
+	outArr = arr.slice(0, optIndex+1);
 	return outArr
 }
 
@@ -115,38 +116,47 @@ function arrSort(arr, sortLists) {
 	}
 	var i = 0;
 	var len = sortLists.length;
-	var inArr = JSON.parse(JSON.stringify(arr));
+	var inArr = [];
 	var outArr = [];
 	if(!len){
-		return inArr;
+		return arr;
 	}
-	// the right method to use arguments.callee in strict mode
-	var sortArrOuter = (function sortArrWrap(arr, sortList) {
-		if(Object.prototype.toString.call(sortList) !== "[object Object]"){
+	// mark array item for follow-up deleting
+	arr.forEach((item, index) => {
+		if(Object.prototype.toString.call(item) !== "[object Object]"){
 			throw new TypeError('PARAM MUST BE OBJECT ARRAY');
 		}
+		item.$$index = index
+		inArr.push(item)
+	});
+	var isSort = false
+	// the right method to use arguments.callee in strict mode
+	var sortArrOuter = (function sortArrWrap(arr, sortList) {
 		var filterArr = [];
 		if (arr.length === 0) {
 			return;
 		}
-		filterArr = selObjArr(arr, sortList.attr || '', sortList.asc);
+		filterArr = selObjArr(arr, sortList.attr || '', sortList.asc, sortList.attr === sortLists[0].attr && isSort);
+		if (!isSort && sortList.attr === sortLists[0].attr) {
+			isSort = true
+		}
 		if(filterArr.length === 0){
 			filterArr = arr;
 		}
 		if (filterArr.length === 1 || i >= len - 1) {
 			outArr = outArr.concat(filterArr);
 			// delete the corresponding original array element
+			var newInArr = []
+			var filterIndexArr = []
 			for(var k=0,len1=filterArr.length; k<len1; k++){
-				// update stringifyInArr in case same elemets cause error deletion
-				var stringifyInArr = [];
-				for(var j=0,len2=inArr.length; j<len2; j++){
-					stringifyInArr.push(JSON.stringify(inArr[j]));
-				}
-				var delIndex = stringifyInArr.indexOf(JSON.stringify(filterArr[k]));
-				if (delIndex !== -1) {
-					inArr = arrDel(inArr, [delIndex]);
+				filterIndexArr.push(filterArr[k].$$index)
+			}
+			for(var j=0,len2=inArr.length; j<len2; j++){
+				if (filterIndexArr.indexOf(inArr[j].$$index) === -1) {
+					newInArr.push(inArr[j])
 				}
 			}
+			inArr = newInArr
 		} else {
 			i++;
 			sortArrWrap(filterArr, sortLists[i])
@@ -161,7 +171,10 @@ function arrSort(arr, sortLists) {
 		loopSortArrWrap();
 	})
 	loopSortArr();
-	return outArr;
+	outArr.forEach(item => {
+		delete item.$$index
+	});
+	return outArr
 }
 
 module.exports = arrSort
